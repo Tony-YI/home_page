4a9800b4965be8bdf908fb5ac10b7181e85c560b
tony-yi <455054421@qq.com>
2014-12-03 16:23:32 +0800

    test

--- project/2014_fall/project_1/ENGG5405 Assignment 6/genCityLocation.m
+++
@@ -1,9 +0,0 @@
-% This function generates the city locations and
-% make sure all the city have different locations [x y]
-
-function xy = genCityLocation(num_city)
-bound = 2 .* num_city;
-x = randperm(bound, num_city);
-y = randperm(bound, num_city);
-xy = [x' y'];
-end
